05e10dafaf26d1ab2849145c76d11a282789720d
roity <31123223+roity@users.noreply.github.com>
2018-10-04 08:20:39 +0530

    path changes

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -29,7 +29,7 @@ public class Hook extends BaseUtil{
 
 
         //Chrome driver
-        System.setProperty("webdriver.chrome.driver", "/home/rajeev/Downloads/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/tmp/chromedriver");
         base.Driver = new ChromeDriver();
     }
 
